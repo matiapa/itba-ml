039c6f52a5e84eda516d6ad13ada46981e81bf1b
Anita <acruz@itba.edu.ar>
2022-08-30 20:09:31 -0300

    additional ej2, bad categorization always into nan

--- TP1/ej2.py
+++ TP1/ej2.py
@@ -62,8 +62,8 @@ def fill_dictionary(df):
    #hash with keys being pair word category
    #and values being the number of times the word appears in the category.
    #for example: {('hola', 'Deportes'): 2, ('hola', 'Economía'): 1} 
-    dictionary = {}
-    total_words_by_category = {}
+    dictionary = {} 
+    total_words_by_category = {} #Deportes: 100, Economía: 50
     total_words = 0
     for index, row in df.iterrows():
         for word in row['title'].split(' '):
@@ -82,21 +82,27 @@ def fill_dictionary(df):
     return dictionary, total_words_by_category, total_words
 
 
-def analyze_title(all_categories, title, dictionary, total_words_by_category, total_words):
+def analyze_title(all_categories, title, dictionary, total_words_by_category, total_words, negative_probabilites_by_category):
     #return the probability of the title being in the category.
     valor_de_clasificacion_por_categoria = {}
     for category in all_categories:
-        valor_de_clasificacion_por_categoria[category] = 0
+        #P(A and not B) P(not A and not B) * P(A)/P(not A), so i'll start by multiplying the probabilities of the words 
+        # in the title not being in the category, and then multiply the second part for each word
+        valor_de_clasificacion_por_categoria[category] = negative_probabilites_by_category[category]
+
         for word in title.split(' '):
             if (word, category) in dictionary:
                 #using laplace smoothing
-                if valor_de_clasificacion_por_categoria[category] == 0:
-                    valor_de_clasificacion_por_categoria[category] = (dictionary[(word, category)] + 1) / (total_words_by_category[category] + all_categories.size)
-                else:
-                    valor_de_clasificacion_por_categoria[category] *= ((dictionary[(word, category)]+1)/(total_words_by_category[category] + all_categories.size)) 
+                prob_of_word = ((dictionary[(word, category)]+1) / (total_words_by_category[category] + all_categories.size))
+                valor_de_clasificacion_por_categoria[category] *= (prob_of_word / (1-prob_of_word))
+            else:
+                #this isn't precise, but its good enough for this exercise. Ines knows what this compromise is
+                #laplace's minimum probability instead of multiplying by 0 or 1.
+                valor_de_clasificacion_por_categoria[category] *= (1 / (total_words_by_category[category] + all_categories.size))
+
+        #times the probability of the category
         valor_de_clasificacion_por_categoria[category] *= (total_words_by_category[category]/total_words)
 
-    print("valor_de_clasificacion_por_categoria: " + str(valor_de_clasificacion_por_categoria))
     denominator = 0
     for value in valor_de_clasificacion_por_categoria.values():
         denominator += value
@@ -114,29 +120,45 @@ def analyze_title(all_categories, title, dictionary, total_words_by_category, to
         if probabilities_by_category[category] > max_probability:
             max_probability = probabilities_by_category[category]
             category_with_max_probability = category
-    print("The category with the highest probability is: " + str(category_with_max_probability) + " with a probability of " + str(max_probability))
+    print("The title is: " + title + " and the category is: " + str(category_with_max_probability))
+    print( " with a probability of " + str(max_probability))
     return category_with_max_probability
 
 
 
 
-def get_accuracy(all_categories, dictionary, total_words_by_category, total_words, test):
+def get_accuracy(all_categories, dictionary, total_words_by_category, total_words, test, negative_probabilites_by_category):
     #return the accuracy of the classifier.
     correct_predictions = 0
     total_predictions = 0
     for index, row in test.iterrows():
         total_predictions += 1
-        if analyze_title(all_categories, row['title'], dictionary, total_words_by_category, total_words) == row['category']:
+        if analyze_title(all_categories, row['title'], dictionary, total_words_by_category, total_words, negative_probabilites_by_category) == row['category']:
             correct_predictions += 1
     return correct_predictions/total_predictions
 
 
+def get_negative_probability(dictionary, total_words_by_category, total_words, category):
+    # multiplication of the probabilities of every word in the dictionary not being in the category.
+    negative_probabilites_dictionary = {} #entretenimiento, salud, economia
+    for word in dictionary:
+        #hola, economia
+        if word[1] in negative_probabilites_dictionary:
+            negative_probabilites_dictionary[word[1]] *= (1- (dictionary[word]/total_words_by_category[word[1]]))
+        else:
+            #P(-word | category)
+            negative_probabilites_dictionary[word[1]] = (1- (dictionary[word]/total_words_by_category[word[1]]))
+    print("negative_probabilites_dictionary: " + str(negative_probabilites_dictionary))
+    return negative_probabilites_dictionary
+        
 
 
+#df = noticias_argentinas_parser("data/Noticias_argentinas_no_extra_columns.csv")
+#train, test = divide_dataset(df, 0.8)
+#dictionary, total_words_by_category, total_words = fill_dictionary(train)
 
-df = noticias_argentinas_parser("data/Noticias_argentinas_no_extra_columns.csv")
-train, test = divide_dataset(df, 0.8)
-dictionary, total_words_by_category, total_words = fill_dictionary(train)
+#negative_probabilities_by_category = get_negative_probability(dictionary, total_words_by_category, total_words, 'Deportes')
+
 #analyze_title(get_categories(df), "Santiago Bal expuso a Carmen Barbieri en vivo contándole por qué le fue infiel", dictionary, total_words_by_category, total_words)
-get_accuracy(get_categories(df), dictionary, total_words_by_category, total_words, test)
+#get_accuracy(get_categories(df), dictionary, total_words_by_category, total_words, test)
 
