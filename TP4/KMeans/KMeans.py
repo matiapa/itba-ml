c0e53c6b270e937b0d380d90ecdb52bb21b88151
inesmarca <imarcarian@itba.edu.ar>
2022-11-21 14:47:20 -0300

    added more pre analysis, added variable analysis of kmeans

--- TP4/KMeans/KMeans.py
+++ TP4/KMeans/KMeans.py
@@ -9,15 +9,21 @@ class KMeans:
         self.classes = {}
 
 
-    def fit(self, X):
-        for i in range(self.k):
-            self.centroids[i] = np.array(X[i][:-1])
+    def fit(self, X, stop=10e-6):
+        random_indexes = np.random.choice(len(X), self.k, replace=False)
+        index = 0
+        # calculate initial centroids
+        for i in random_indexes:
+            self.centroids[index] = np.array(X[i][:-1])
+            index += 1
 
         for i in range(self.max_iter):
+            # clean classes
             self.classes = {}
-            for i in range(self.k):
-                self.classes[i] = []
+            for r in range(self.k):
+                self.classes[r] = []
 
+            # assign each row to a class based the nearest euclidian distance
             for features in X:
                 # remove the first element of the features array, which is the imdb_id
                 aux = np.array(features[:-1])
@@ -25,12 +31,12 @@ class KMeans:
                 classification = distances.index(min(distances))
                 self.classes[classification].append(features)
 
+            # calculate new centroids
             prev_centroids = dict(self.centroids)
-
             for classification in self.classes:
-                # create a numpy array with the first element of each row of the classes[classification] array
-                # this is the vote_average column
+                # remove imdb_id from the features
                 aux = np.array([np.array(row[:-1]) for row in self.classes[classification]])
+                # caculate the mean of the features
                 self.centroids[classification] = np.average(aux, axis=0)
 
             optimized = True
@@ -38,7 +44,7 @@ class KMeans:
             for c in self.centroids:
                 original_centroid = prev_centroids[c]
                 current_centroid = self.centroids[c]
-                if np.linalg.norm(current_centroid - original_centroid) > 0.0001:
+                if np.linalg.norm(current_centroid - original_centroid) > stop:
                     optimized = False
 
             if optimized:
--- TP4/KMeans/main.py
+++ TP4/KMeans/main.py
@@ -13,41 +13,151 @@ def variation(classes):
                 for p in range(len(row1)):
                     sum += (row1[p] - row2[p])**2
         sum *= 1/len(classes[c])
-        print("Class", c, "variation:", sum)
+        # print("Class", c, "variation:", sum)
 
         class_variation[c] = sum
 
     sum = 0
     for c in class_variation:
         sum += class_variation[c]
+
     return sum
 
-
-def main(columns=None, id_column='imdb_id', k=3, max_iter=100, write_to_file=False):
+def k_similarity_analysis():
+    id_column = 'imdb_id'
     df_proc = pd.read_csv('../data/movie_data_proc.csv')
-    df = pd.read_csv('../data/movie_data.csv', sep=';')
+    df = pd.read_csv('../data/movie_data.csv')
+    columns = df_proc.columns.drop(id_column).to_list()
+    columns.append(id_column)
+    df_proc = df_proc[columns]
+    X = df_proc.to_numpy()
+
+    ks = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    similarities = []
+    cant = 10
+    for k in ks:
+        print("-------- K:", k)
+        kmeans = KMeans(k=k, max_iter=100)
+        aux = []
+        for i in range(cant):
+            kmeans.fit(X)
+            centroid = kmeans.centroids
+            classes = kmeans.classes
+
+            similarity = variation(classes)/len(classes)
+            aux.append(similarity)
+            print("Similarity:", similarity)
+        # get the min aux value
+        print("k =", k, "similarity:", min(aux))
+        similarities.append(min(aux))
+
+    print("Similarities:", similarities)
+    plt.plot(ks, similarities, marker='o')
+    plt.xlabel('k')
+    plt.ylabel('Similitud')
+    plt.show()
+
+def accuracy(classes, df):
+    correct = 0
+    total = 0
+    for c in classes:
+        genres = []
+        for row in classes[c]:
+            id = row[-1]
+            genres.append(df[df['imdb_id'] == id]['genres'].to_list()[0])
+        mode = max(set(genres), key=genres.count)
+        correct += genres.count(mode)
+        total += len(genres)
+
+    return correct/total
+
+
+
+def k_accuracy_analysis():
+    id_column = 'imdb_id'
+    df_proc = pd.read_csv('../data/movie_data_proc.csv')
+    df = pd.read_csv('../data/movie_data.csv')
+    columns = df_proc.columns.drop(id_column).to_list()
+    columns.append(id_column)
+    df_proc = df_proc[columns]
+    X = df_proc.to_numpy()
+
+    ks = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    accuracies = []
+    cant = 10
+    for k in ks:
+        print("-------- K:", k)
+        kmeans = KMeans(k=k, max_iter=100)
+        aux = []
+        for i in range(cant):
+            kmeans.fit(X)
+            centroid = kmeans.centroids
+            classes = kmeans.classes
+            aux.append(accuracy(classes, df))
+            print("Accuracy:", aux[-1])
+        print("k =", k, "accuracy:", max(aux))
+        accuracies.append(max(aux))
+
+    print("Accuracies:", accuracies)
+    plt.plot(ks, accuracies, marker='o')
+    plt.xlabel('k')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+
+
+def main(columns=None, id_column='imdb_id', k=3, max_iter=100, cant=15, type='variation', write_to_file=False):
+    if type not in ['variation', 'accuracy']:
+        print("Invalid type")
+        return
+
+    df_proc = pd.read_csv('../data/movie_data_proc.csv')
+    df = pd.read_csv('../data/movie_data.csv')
     if columns is None:
         columns = df_proc.columns.drop(id_column).to_list()
     columns.append(id_column)
     df_proc = df_proc[columns]
-
     X = df_proc.to_numpy()
-    kmeans = KMeans(k=k, max_iter=max_iter)
-    kmeans.fit(X)
 
-    centroid = kmeans.centroids
-    classes = kmeans.classes
+    kmeans = KMeans(k=k, max_iter=max_iter)
+
+    min_variation = None
+    min_accuracy = None
+    min_centroids = None
+    min_classes = None
+    for i in range(cant):
+        kmeans.fit(X)
+        centroid = kmeans.centroids
+        classes = kmeans.classes
+
+        if type == 'variation':
+            var = variation(classes)
+            if (min_variation is None) or (var < min_variation):
+                min_accuracy = accuracy(classes, df)
+                min_variation = var
+                min_centroids = centroid
+                min_classes = classes
+        else:
+            acc = accuracy(classes, df)
+            if (min_accuracy is None) or (acc > min_accuracy):
+                min_variation = variation(classes)
+                min_accuracy = acc
+                min_centroids = centroid
+                min_classes = classes
+
+    print("Accuracy:", min_accuracy)
+    print("Variation:", min_variation)
 
     if write_to_file:
-        res_file = open('../data/results.csv', 'w')
+        res_file = open('../data/results_'+type+'.csv', 'w')
         res_file.write('class,'+','.join(df.columns) + '\n')
         for i in range(len(classes)):
-            for row in classes[i]:
+            for row in min_classes[i]:
                 res_file.write(str(i)+','+df[df['imdb_id'] == row[-1]].to_csv(header=None, index=False).split('\n')[0] + '\n')
-
         res_file.close()
 
-    print("Calculating variation...")
-    print(variation(classes))
+columns=['budget','production_countries','revenue','runtime','spoken_languages','vote_average','vote_count']
+main(k=5, max_iter=1000, type='accuracy', columns=columns, write_to_file=True)
+# k_similarity_analysis()
+# k_accuracy_analysis()
 
-main(k=20, write_to_file=True)
